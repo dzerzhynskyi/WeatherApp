7f2529c9b33b0a52809636ddaad8a00a38e442e9
vladyslavdzerzhynskyi <dzerzhynskyi@gmail.com>
2024-12-15 20:42:42 -0500

    Initial commit

--- Main.py
+++ Main.py
@@ -1,9 +1,8 @@
 import customtkinter as ctk
 from settings import *
-
+import requests
 import urllib.request
 import json
-
 
 try:
     from ctypes import windll, byref, sizeof, c_int
@@ -29,20 +28,33 @@ class App(ctk.CTk):
 
 
 if __name__ == '__main__':
-
-#setting the IPA and getting the weather data
+    # setting the IPA and getting the weather data
     def get_weather(latitude, longitude, units, period):
         full_url = f"{BASE_URL}&lat={latitude}&lon={longitude}&appid={API_KEY}&units={units}"
-        print(full_url)
+        response = requests.get(full_url)
 
+        current_data = {}
+
+        if response.status_code == 200:
+            data = response.json()
+            for key, value in data.items():
+                if key == "list":
+                    for index, data_entry in enumerate(value):
+                        if index == 0:
+                            current_data['temp'] = int(round(data_entry['main']['temp'], 0))
+                            current_data['feels_like'] = int(round(data_entry['main']['feels_like'], 0))
+                            current_data['weather'] = data_entry['weather'][0]['main']
+        if period == 'today':
+            return current_data
 
 # weather for New York
-get_weather(latitude=40.7128, longitude=74.0060, units="metric", period="today")
+current_data = get_weather(latitude=40.7127281, longitude=-74.0060152, units="metric", period="today")
+print(current_data)
 
 if __name__ == '__main__':
     # get location
     # does not work
-    #with urllib.request.urlopen("https://ipapi.co/json/") as url:
-        #data = json.loads(url.read().decode())
+    # with urllib.request.urlopen("https://ipapi.co/json/") as url:
+    # data = json.loads(url.read().decode())
 
     App(app_width=500, app_height=350)
